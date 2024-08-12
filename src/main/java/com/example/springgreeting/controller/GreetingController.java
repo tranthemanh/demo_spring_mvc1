7419b7a47a0531aa9232073675d68bcacabc3261
tranthemanh <tranmanhhuha@gmail.com>
2024-08-12 14:11:41 +0700

    lan 1

---
+++ src/main/java/com/example/springgreeting/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.example.springgreeting.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting(Model model) {
+        model.addAttribute("name", "Manh");
+        return "demo";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/demo.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: admin
+  Date: 8/12/2024
+  Time: 10:26 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Demo</h1>
+<h2>${name}</h2>
+</body>
+</html>
